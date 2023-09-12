20fd704fded1fdf6c4d6b11d3dac7abd4bd1b60e
nnb9797 <108178648+nnb9797@users.noreply.github.com>
2023-09-12 17:55:51 +0700

    first commit

--- HW_1/HW_test_1.py
+++ HW_1/HW_test_1.py
@@ -7,17 +7,16 @@
 import subprocess
 from typing import List
 
-def text_checking(command: List[str], text: str) -> bool:
-    result_command: str = (subprocess.run(command, stdout=subprocess.PIPE,
+
+def check_text_function(command: List[str], text: str) -> bool:
+    search_command: str = (subprocess.run(command, stdout=subprocess.PIPE,
                                           text=True, shell=True, encoding='utf-8')).stdout
 
-    return text in result_command
+    return text in search_command
 
 
 if __name__ == '__main__':
-    # command_list: List[str] = ["cat /etc/os-release"]
-    command_list: List[str] = ["cat test_2.py"]
-    # text_search: str = 'https://help.ubuntu.com/'
-    text_search: str = '22.04.3'
-    text_was_found: bool = text_checking(command_list, text_search)
-    print(f'\nВывод: \n{text_search}\n{text_was_found}')
+    test_command: List[str] = ["cat HW_test1.py"]
+    text_search: str = "Linux"
+    search_result: bool = check_text_function(test_command, text_search)
+    print(f'Text for search: \n{text_search}\nSearch result:\n{search_result}')
